21cc2eec0041d8173230a426a838bb2a5c1b7c93
maggiey8 <144460257+maggiey8@users.noreply.github.com>
2024-05-09 10:10:34 -0400

    spotify login fixed

--- snoopy2/src/App.vue
+++ snoopy2/src/App.vue
@@ -4,17 +4,6 @@ import HelloWorld from './components/HelloWorld.vue'
 import { ref, onMounted } from 'vue'
 import { supabase } from './lib/supabaseClient'
 
-const countries = ref([])
-
-async function getCountries() {
-  const { data } = await supabase.from('countries').select()
-  countries.value = data
-}
-
-onMounted(() => {
-  getCountries()
-})
-
 </script>
 
 <template>
@@ -32,8 +21,8 @@ onMounted(() => {
   </header>
   <RouterView />
 
-  <ul>
-    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-  </ul>
-
 </template>
+
+<style>
+
+</style>

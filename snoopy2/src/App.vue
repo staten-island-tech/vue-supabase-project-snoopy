c69bd2fce1e1246ed74954ab2394822bc82b81b6
maggiey8 <144460257+maggiey8@users.noreply.github.com>
2024-05-22 10:13:53 -0400

    moving over to vue, update for loop playlists

--- snoopy2/src/App.vue
+++ snoopy2/src/App.vue
@@ -1,7 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import { ref, onMounted } from 'vue'
 
 </script>
 
@@ -10,8 +8,6 @@ import { ref, onMounted } from 'vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>

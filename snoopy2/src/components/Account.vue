65b93f9541c2b39083fb7f59c0cffe74485e025c
cindyf21 <144460360+cindyf21@users.noreply.github.com>
2024-06-02 23:57:16 -0400

    add

---
+++ snoopy2/src/components/Account.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { onMounted, ref, toRefs } from 'vue'
+import { supabase } from '@/lib/supabaseClient'
+
+const props = defineProps(['session'])
+const { session } = toRefs(props)
+
+const loading = ref(true)
+const username = ref('')
+
+onMounted(() => {
+  getProfile()
+})
+
+</script>
+
+
+<template>
+    <form>
+      <div>
+        <label for="email">Email</label>
+        <input id="email" type="text" :value="session.user.email" disabled />
+      </div>
+      <div>
+        <label for="username">Name</label>
+        <input id="username" type="text" v-model="username" />
+      </div>
+  
+      <div>
+        <input
+          type="submit"
+          :value="loading ? 'Loading ...' : 'Update'"
+          :disabled="loading"
+        />
+      </div>
+  
+      <div>
+        <button @click="signOut" :disabled="loading">Sign Out</button>
+      </div>
+    </form>
+  </template>

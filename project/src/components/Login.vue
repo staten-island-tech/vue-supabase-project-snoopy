b510842d788c541f40aef991033ff48726a12126
maggiey8 <144460257+maggiey8@users.noreply.github.com>
2024-06-04 21:33:10 -0400

    login actually works ???

--- project/src/components/Login.vue
+++ project/src/components/Login.vue
@@ -1,26 +1,55 @@
 <template>
-    <div>
-    <input type="text"></input>
-      <button @click="login" v-if="!store.logged">Sign In</button>
-      <button @click="signOut" v-if="store.logged">Sign Out</button>
-    </div>
+    <div v-if="!store.logged">
+      <label for="email">Email  </label>
+    <input id="email" type="text" v-model="email"></input>
+    <label for="password">Password  </label>
+    <input id="password" type="text"v-model="password"></input>
+      <button @click="signIn">Sign In</button>
+      <button @click="signUp">Sign Up</button>
+  
+    </div>    
+    <button @click="signOut" v-if="store.logged">Sign Out</button>
   </template>
   
   <script setup>
-  import { supabase } from '../../../supabaseClient';
-  import { useStore } from '@/stores/counter.js';
+  import { supabase } from '../../supabase';
+  import { useStore } from '@/stores/counter';
+  import { ref } from 'vue';
+  const store = useStore()
 
-  
-  async function login() {
-    await signInWithSpotify();
-    auth()
+  const email = ref("")
+  const password = ref("")
+
+  async function signUp() {
+    let { data, error } = await supabase.auth.signUp({
+    email: email.value,
+    password: password.value,    
+  })
+  if (error) {
+    console.log(error)
   }
-  
+  else {
+    signIn()
+    store.logged = true
+  }
+  }
+
+  async function signIn() {
+    let { data, error } = await supabase.auth.signInWithPassword({
+  email: email.value,
+  password: password.value,
+  })  
+  if (error) {
+    console.log(error)
+  }
+  else {
+    store.logged = true
+  }
+}
+
   async function signOut() {
-    const { error } = await supabase.auth.signOut();
-    if (error) {
-      console.error(error);
-    }
+    supabase.auth.signOut();
+    store.logged = false
   }
   
   

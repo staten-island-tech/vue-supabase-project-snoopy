5e4ec02210c8191610d814e688d1019284e5d4f2
maggiey8 <144460257+maggiey8@users.noreply.github.com>
2024-06-04 22:47:16 -0400

    hold on were getting somewhere

--- project/src/stores/counter.js
+++ project/src/stores/counter.js
@@ -1,10 +1,11 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
+import { supabase } from '../../supabase'
 
 export const useStore = defineStore('store' ,async () => {
 
 const logged = ref(false)
-const profile = ref(null)
+const token = ref(null)
 
-  return {logged, profile}
+  return {logged, token}
 })
---
+++ project/src/views/CreatePlaylist.vue
@@ -0,0 +1,33 @@
+<template>
+    <Login v-if="store.logged"></Login>
+    <div v-if="store.logged">
+        <div>
+        <h2>Create New Playlist</h2>
+        <label for="search">Search for Tracks </label>
+        <input type="text" id="search" v-model="searchQuery">
+        <button @click="search_tracks">Submit</button>
+    </div>
+    <div>
+        <input type="text" placeholder="Playlist Name"></input>
+    </div>
+    </div>
+
+</template>
+
+<script setup>
+import Login from '@/components/Login.vue';
+import { useStore } from '@/stores/counter';
+import { ref } from 'vue';
+
+const store = useStore()
+const searchQuery = ref("")
+
+async function search_tracks() {
+    console.log(searchQuery._rawValue)
+}
+
+</script>
+
+<style scoped>
+
+</style>
